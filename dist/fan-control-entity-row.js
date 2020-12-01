bddfbfd6c598369d2068e855fd9c7f7461e5ff92
Tray Torrance <torrancew@gmail.com>
2020-11-30 16:29:12 -0800

    Swap ordering of `fan.turn_off` and `fan.set_speed`
    Fixes #36

--- dist/fan-control-entity-row.js
+++ dist/fan-control-entity-row.js
@@ -260,8 +260,8 @@ class CustomFanRow extends Polymer.Element {
 	setSpeed(e) {
 		const speed = e.currentTarget.getAttribute('name');
 		if( speed == 'off' ){
-			this.hass.callService('fan', 'turn_off', {entity_id: this._config.entity});
 			this.hass.callService('fan', 'set_speed', {entity_id: this._config.entity, speed: speed});
+			this.hass.callService('fan', 'turn_off', {entity_id: this._config.entity});
 		} else {
 			if(this._config.sendStateWithSpeed){
 			this.hass.callService('fan', 'turn_on', {entity_id: this._config.entity});
